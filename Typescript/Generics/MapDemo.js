bdd388bd2c0cec9b1ec9720e33b0026ed7ec4a28
kokaneva <kokane.vina@gmail.com>
2023-08-16 17:11:46 +0530

    commit

---
+++ Typescript/Generics/MapDemo.js
@@ -0,0 +1,23 @@
+// keys unique : if duplicate then override
+// values can be duplicate
+let myMap = new Map();
+myMap.set(3456, "Pune");
+myMap.set(1111, "Solapur");
+myMap.set(3456, "Mumbai");
+myMap.set(4444, "Solapur");
+myMap.set(5454, "Delhi");
+myMap.set(6778, "Thane");
+myMap.set(7887, "Thane");
+console.log(myMap);
+let keyArray = myMap.keys();
+console.log(keyArray);
+let valueArray = myMap.values();
+console.log(valueArray);
+// Employees living in which dfftt cities
+let citySet = new Set(valueArray);
+console.log(citySet);
+myMap.delete(7887);
+let city = myMap.get(1111);
+console.log(`employee 1111 is living in city ${city}`);
+console.log(myMap);
+// iteraration via entries
