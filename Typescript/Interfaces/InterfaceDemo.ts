d3bec7ef884cf7317348a2c70515090cf9b094f1
kokaneva <kokane.vina@gmail.com>
2023-08-16 15:19:27 +0530

    commit

---
+++ Typescript/Interfaces/InterfaceDemo.ts
@@ -0,0 +1,30 @@
+interface EmployeeIntf{
+    salary:number;    // variable sig
+    getSalary():number;   // method sig
+    ta?:number;
+    getTA?():number;
+}
+
+// class must override the methods declared in the interfaces
+class NeoEmployee implements EmployeeIntf{
+    salary:number;
+    ta:number;
+    getSalary():number{
+        return this.salary+5670+3456;
+    }
+    getTA():number{
+        return 0;
+    }
+}
+
+class SquadEmployee implements EmployeeIntf{
+    salary:number;
+    getSalary():number{
+        return this.salary+345+2222;
+    }   
+}
+
+let emp:EmployeeIntf;
+emp=new NeoEmployee();
+
+emp=new SquadEmployee();
