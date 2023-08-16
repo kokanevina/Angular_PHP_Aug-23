c1079273d2efaa7a4f3ae8a01d190037eb5798e6
kokaneva <kokane.vina@gmail.com>
2023-08-16 17:34:41 +0530

    commit

--- Typescript/Generics/CustomGenericDemo.ts
+++ Typescript/Generics/CustomGenericDemo.ts
@@ -1,5 +1,4 @@
 
-
 class Employee<T>{
     empId:T
     constructor(empId:T){
@@ -15,3 +14,24 @@ console.log(emp1.getEmpId());
 
 let emp2=new Employee<string>("NEO123");
 console.log(emp2.getEmpId());
+
+
+
+interface StudIntf<T>{
+    studId:T
+}
+
+
+class NeoStudent implements StudIntf<string>{
+    studId:string
+}
+
+class SquadStudent implements StudIntf<number>{
+    studId:number
+}
+
+let neo1=new NeoStudent();
+let neo2=new NeoStudent();
+let neo3=new NeoStudent();
+
+let squad1=new SquadStudent();
