0d399eea25eaee6af489572036d67242f112e97f
kokaneva <kokane.vina@gmail.com>
2023-08-21 12:51:01 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/employee-card/employee-card.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/employee-card/employee-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Employee } from '../classes/employee';
 
 @Component({
@@ -8,6 +8,19 @@ import { Employee } from '../classes/employee';
 })
 export class EmployeeCardComponent {
 
-  @Input()
+  @Input() // input coming from parent 
   employee:Employee=new Employee();
+
+ 
+  companyName="Seed Infotech Ltd.";   //1. declare the data to give to parent
+
+  @Output()  // output going to parent
+  emitter=new EventEmitter<string>();   //2.
+  // emitting event : lifecycle methods, function
+  
+  // component lifecycle method  (constructors dont emit events)
+  ngOnInit(){
+    console.log("EmployeeCard lifecycle method");
+    this.emitter.emit(this.companyName);  //3.
+  }
 }
--- Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
@@ -13,5 +13,18 @@ export class EmployeeComponent {
     this.employees.push(new Employee(111,"Niraj Salvi",92000,"PHP Developer",new Date('1 Jun 2009'),"assets/Images/e111.jpg"));
     this.employees.push(new Employee(434,"Amol Puranik",56000,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
   }
+
+  getEmployees(){
+    // here u will get the data from service
+  }
+
+  cname="";
+  receiver(ev:string){
+    console.log("-----receiver function called----");
+    
+    console.log(ev);
+    this.cname=ev;
+    
+  }
 }
 
