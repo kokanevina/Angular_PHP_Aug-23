b522970a894869224b449efbca9d1f1de6daa3d9
kokaneva <kokane.vina@gmail.com>
2023-08-21 14:38:27 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
@@ -8,10 +8,10 @@ import { Employee } from '../classes/employee';
 export class EmployeeComponent {
   employees=new Array<Employee>();
   constructor(){
-    this.employees.push(new Employee(321,"Kiran Patil",67000,"PHP Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
-    this.employees.push(new Employee(123,"Ravi Kumar",87000,"Java Developer",new Date('12 Jun 2009'),"assets/Images/e123.jpg"));
-    this.employees.push(new Employee(111,"Niraj Salvi",92000,"PHP Developer",new Date('1 Jun 2009'),"assets/Images/e111.jpg"));
-    this.employees.push(new Employee(434,"Amol Puranik",56000,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
+    this.employees.push(new Employee(321,"Kiran rana Patil",67000,"php Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
+    this.employees.push(new Employee(123,"ravi Kumar",87000,"java Developer",new Date('12 Jun 2009'),"assets/Images/e123.jpg"));
+    this.employees.push(new Employee(111,"Niraj SalvI",92000,"PHP Developer",new Date('1 Jun 2009'),"assets/Images/e111.jpg"));
+    this.employees.push(new Employee(434,"AmOl Puranik",56000,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
   }
   getEmployees(){
     // here u will get the data from service
