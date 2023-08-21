40aa55dd2e78f21e311f4c644202a582fc91fd41
kokaneva <kokane.vina@gmail.com>
2023-08-21 15:17:07 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
@@ -8,10 +8,12 @@ import { Employee } from '../classes/employee';
 export class EmployeeComponent {
   employees=new Array<Employee>();
   constructor(){
-    this.employees.push(new Employee(321,"Kiran rana Patil",67000,"php Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
-    this.employees.push(new Employee(123,"ravi Kumar",87000,"java Developer",new Date('12 Jun 2009'),"assets/Images/e123.jpg"));
-    this.employees.push(new Employee(111,"Niraj SalvI",92000,"PHP Developer",new Date('1 Jun 2009'),"assets/Images/e111.jpg"));
-    this.employees.push(new Employee(434,"AmOl Puranik",56000,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
+    this.employees.push(new Employee(321,"Kiran rana Patil",67000.64578,"php Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
+    this.employees.push(new Employee(321,"sudhakar puri",67000,"python Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
+    this.employees.push(new Employee(123,"ravi Kumar",87000.4365,"java Developer",new Date('12 Jun 2009'),"assets/Images/e123.jpg"));
+    this.employees.push(new Employee(111,"Niraj SalvI",92000.786,"PHP Developer",new Date('1 Jun 2009'),"assets/Images/e111.jpg"));
+    this.employees.push(new Employee(434,"AmOl Puranik",56000.33,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
+    this.employees.push(new Employee(134,"Suman Puranik",1356000.7,"Python Developer",new Date('13 Aug 2015'),"assets/Images/e434.jpg"));
   }
   getEmployees(){
     // here u will get the data from service
@@ -22,5 +24,6 @@ export class EmployeeComponent {
     console.log(ev);
     this.cname=ev;
   }
+  today=new Date();
 }
 
