9db98dce6938fb10ee62742c3574561fa94d012f
kokaneva <kokane.vina@gmail.com>
2023-08-22 15:34:53 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/register/register.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,15 +7,31 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterComponent {
    registerForm:FormGroup;
+   namePattern="[A-Za-z ]*";
+   nameP=new RegExp("[A-Za-z ]*");
   constructor(){ 
     this.registerForm=new FormGroup({
-      customerName:new FormControl(),
-      customerEmail:new FormControl(),
-      customerAge:new FormControl(),
-      customerMobile:new FormControl(),
-      customeruname:new FormControl(),
-      customerPassword:new FormControl(),
-      customerConfirmPassword:new FormControl()
+      customerName:new FormControl("",[Validators.required, Validators.pattern(this.namePattern)]),
+      customerEmail:new FormControl("",[Validators.required]),
+      customerAge:new FormControl("",[Validators.required]),
+      customerMobile:new FormControl("",[Validators.required]),
+      customeruname:new FormControl("",[Validators.required]),
+      customerPassword:new FormControl("",[Validators.required]),
+      customerConfirmPassword:new FormControl("",[Validators.required])
     });
   }
+
+  get cname(){
+    return this.registerForm.get('customerName');
+  }
 }
+
+/*
+   requiredValidator(){
+    to test whether the field is empty or not
+    if(field is not empty)
+      return null
+    else
+      { 'required':true}
+   }
+*/
