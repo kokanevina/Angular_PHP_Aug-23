e7288c8ec8a661286b605e07332a567c4be42644
kokaneva <kokane.vina@gmail.com>
2023-08-22 13:10:13 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/register/register.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+   registerForm:FormGroup;
+  constructor(){ 
+    this.registerForm=new FormGroup({
+      customerName:new FormControl(),
+      customerEmail:new FormControl(),
+      customerAge:new FormControl(),
+      customerMobile:new FormControl(),
+      customeruname:new FormControl(),
+      customerPassword:new FormControl(),
+      customerConfirmPassword:new FormControl()
+    });
+  }
 }
