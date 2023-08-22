a61586d33fa676ddee90ef142bcb4b7528b1f057
kokaneva <kokane.vina@gmail.com>
2023-08-22 12:41:21 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/app.module.ts
+++ Angular Workspace/employeemanagementproject/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EmployeeCardComponent } from './employee-card/employee-card.component';
 import { LengthPipe } from './pipes/length.pipe';
 import { IndexPipe } from './pipes/index.pipe';
 import { OrderByPipe } from './pipes/order-by.pipe';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { OrderByPipe } from './pipes/order-by.pipe';
     EmployeeCardComponent,
     LengthPipe,
     IndexPipe,
-    OrderByPipe
+    OrderByPipe,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
