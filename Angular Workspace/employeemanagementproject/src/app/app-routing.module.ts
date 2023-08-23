bccf6cdb3e4abf7821c95009d8eb2f5d43ff7820
kokaneva <kokane.vina@gmail.com>
2023-08-23 12:00:30 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/app-routing.module.ts
+++ Angular Workspace/employeemanagementproject/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component:HomeComponent },
+  { path:'myhome', component:HomeComponent },
+  { path:'login', component:LoginComponent},
+  { path:'register', component:RegisterComponent},
+  { path:'employees/showemployees', component:EmployeeComponent},
+  { path:'', component:HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+ }
