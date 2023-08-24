c1ecf005cf98b9c98d8fe4be55dddc617bda7f18
kokaneva <kokane.vina@gmail.com>
2023-08-24 13:13:22 +0530

    commit

--- Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
+++ Angular Workspace/employeemanagementproject/src/app/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Employee } from '../classes/employee';
 import { EmployeeCrudService } from '../services/employee-crud.service';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -9,6 +10,8 @@ import { EmployeeCrudService } from '../services/employee-crud.service';
 export class EmployeeComponent {
   openFlag=false;
   employees=new Array<Employee>();
+  empForm:FormGroup;
+  employee=new Employee();
   constructor(private crudService:EmployeeCrudService ){
    /*  this.employees.push(new Employee(321,"Kiran rana Patil",67000.64578,"php Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
     this.employees.push(new Employee(311,"sudhakar puri",67000,"python Developer",new Date('12 Jun 2010'), "assets/Images/e321.jpg"));
@@ -17,6 +20,14 @@ export class EmployeeComponent {
     this.employees.push(new Employee(12,"AmOl Puranik",56000.33,"Java Developer",new Date('12 May 2015'),"assets/Images/e434.jpg"));
     this.employees.push(new Employee(134,"Suman Puranik",1356000.7,"Python Developer",new Date('13 Aug 2015'),"assets/Images/e434.jpg")); */
     
+    this.empForm=new FormGroup({
+      id:new FormControl(),
+      ename:new FormControl(),
+      esalary: new FormControl(),
+      edesignation: new FormControl(),
+      ejoiningDate: new FormControl()
+    });
+
     crudService.getAllEmployees().subscribe({
       next:(data)=>this.employees=data as Employee[],
       error:(error)=>console.log(error)
@@ -38,6 +49,12 @@ export class EmployeeComponent {
   orderKey="eid";
   sendKey(key:string){
     this.orderKey=key;
+  }
+
+  collectEmp():void{
+    this.employee=this.empForm.value;
+    console.log(this.employee);
+
   }
 }
 
